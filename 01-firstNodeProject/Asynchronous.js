ddfe750c5130cc1e0daaef27b06719982895a849
fhernandezp <108768697+fhernandezp@users.noreply.github.com>
2023-03-10 19:39:08 -0500

    Feature: Events loop and asynchronous code

---
+++ 01-firstNodeProject/Asynchronous.js
@@ -0,0 +1,3 @@
+//this is a simple example of the asynchronous code
+setTimeout(() => console.log('this is the first console log!'));
+console.log('this is the second console log!');
---
+++ 01-firstNodeProject/EventsLoop.js
@@ -0,0 +1,13 @@
+//events loops in libuv are important to execute asynchonous events in node
+//event loops are basically queues
+while (true){
+    //instructions to execute
+}
+
+/**
+ * Event loop phases
+ *  - Timers => there're 3 types of timers : setTimeout, setInterval and setInmediate
+ *  - I/0 Callbacks
+ *  - setInmediate
+ *  - Close callbacks
+ */
