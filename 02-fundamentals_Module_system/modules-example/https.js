923c973eb44c1a0139abbe157241bdd9d341e266
fhernandezp <108768697+fhernandezp@users.noreply.github.com>
2023-03-13 22:15:57 -0500

    Feature: using index.js to export properties

--- 02-fundamentals_Module_system/modules-example/https.js
+++ 02-fundamentals_Module_system/modules-example/https.js
@@ -1,9 +1,9 @@
-const {send} = require('./request');
-const {read} = require('./response');
-
+//const {send} = require('./request');
+//const {read} = require('./response');
+const internals = require('./internals');
 function makeRequest(url, data){
-    send(url, data);
-    return read();
+    internals.request.send(url, data);
+    return internals.response.read();
     
 }
 
