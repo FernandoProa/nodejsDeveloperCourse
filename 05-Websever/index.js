5872ae77e4ad3fd92efd9c8d3e32d34745ecca48
Fernando Proa <45346347+FernandoProa@users.noreply.github.com>
2023-05-18 00:46:24 -0500

    Feat: End of section (Webservers)

--- 05-Websever/index.js
+++ 05-Websever/index.js
@@ -20,19 +20,29 @@ const friends = [
 server.on('request', (req, res) => {
     const items = req.url.split('/');
 
-    if (items[1] === 'friends') {
+    if (req.method === 'POST' && items[1] === 'friends') {
+        req.on('data', (data) => {
+            const friend = data.toString();
+
+            console.log('Request: ', friend);
+            friends.push(JSON.parse(friend));
+        });
+        
+        req.pipe(res);
+
+    } else if (req.method === 'GET' && items[1] === 'friends') {
 
         res.writeHead(200, {
             'Content-Type': 'text/plain',
         })
-        if(items.length === 3){
+        if (items.length === 3) {
             const friendIndex = parseInt(items[2]);
             res.end(JSON.stringify(friends[friendIndex]));
         } else {
             res.end(JSON.stringify(friends));
         }
 
-    } else if (items[1] === 'message') {
+    } else if (req.method === 'GET' && items[1] === 'message') {
         res.setHeader('Content-Type', 'text/plain')
         res.write('messages');
         res.end();
