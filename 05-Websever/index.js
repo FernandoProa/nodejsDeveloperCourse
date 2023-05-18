332c13e1c8449c7592656e2608e0a2210ca55da1
Fernando Proa <45346347+FernandoProa@users.noreply.github.com>
2023-05-17 23:49:30 -0500

    Feat: Basic routing

--- 05-Websever/index.js
+++ 05-Websever/index.js
@@ -1,16 +1,44 @@
 const http = require('http');
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': 'text/plain',
-    })
-    res.end(JSON.stringify({
-        id: 1,
-        name: "Fernando Proa"
-    }));
-});
+const server = http.createServer();
 
+const friends = [
+    {
+        id: 1,
+        name: 'Fernando Proa'
+    },
+    {
+        id: 2,
+        name: 'Isaac Newton'
+    },
+    {
+        id: 3,
+        name: 'Nikola Tesla'
+    }
+];
+server.on('request', (req, res) => {
+    const items = req.url.split('/');
+
+    if (items[1] === 'friends') {
+
+        res.writeHead(200, {
+            'Content-Type': 'text/plain',
+        })
+        if(items.length === 3){
+            const friendIndex = parseInt(items[2]);
+            res.end(JSON.stringify(friends[friendIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+
+    } else if (items[1] === 'message') {
+        res.setHeader('Content-Type', 'text/plain')
+        res.write('messages');
+        res.end();
+    }
+
+});
 server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`)
 });
